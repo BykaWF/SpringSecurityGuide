b1c9f6e0c440fd347eee5d30f44b97204b5b719c
BykaWF <yaroslavprozorov213@gmail.com>
2023-09-17 18:05:00 +0200

    Created Template for Guide

---
+++ src/main/java/Guide/SecuritySpring/contoller/UserController.java
@@ -0,0 +1,9 @@
+package Guide.SecuritySpring.contoller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class UserController {
+}
