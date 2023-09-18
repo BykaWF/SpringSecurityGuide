c82ea1696e3ab69787907c66e1686b6416c4a149
BykaWF <yaroslavprozorov213@gmail.com>
2023-09-18 09:07:06 +0200

    Added SecurityConfig and UserController

--- src/main/java/Guide/SecuritySpring/config/SecurityConfig.java
+++ src/main/java/Guide/SecuritySpring/config/SecurityConfig.java
@@ -1,10 +1,29 @@
 package Guide.SecuritySpring.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    /**
+     *  @Explanation: anyRequest() should be authenticated() and for getting pop up Basic Auth you need add httpBasic()
+     *
+     *  @Note: You can't log out after you have authenticated
+     */
+    @Bean
+    public SecurityFilterChain formLogin(HttpSecurity http) throws Exception{
+        http.
+                authorizeHttpRequests(authorize -> authorize
+                        .anyRequest()
+                        .authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
 }
