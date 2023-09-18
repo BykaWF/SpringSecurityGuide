8455c17d051280a65cee178f5d69f8ce1df14025
BykaWF <yaroslavprozorov213@gmail.com>
2023-09-18 13:14:00 +0200

    Added new configuration to SecurityConfig with permitAll()

--- src/main/java/Guide/SecuritySpring/config/SecurityConfig.java
+++ src/main/java/Guide/SecuritySpring/config/SecurityConfig.java
@@ -1,29 +1,32 @@
 package Guide.SecuritySpring.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     /**
-     *  @Explanation: anyRequest() should be authenticated() and for getting pop up Basic Auth you need add httpBasic()
+     *  @Explanation: if your request is matching ("/") to give permit it without authentication. If request is not matched it should be authenticated by Basic Auth
+     *
      *
      *  @Note: You can't log out after you have authenticated
      */
     @Bean
-    public SecurityFilterChain formLogin(HttpSecurity http) throws Exception{
-        http.
-                authorizeHttpRequests(authorize -> authorize
-                        .anyRequest()
-                        .authenticated()
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> {
+                            auth.requestMatchers("/").permitAll();
+                            auth.anyRequest().authenticated();
+                        }
                 )
-                .httpBasic(Customizer.withDefaults());
-        return http.build();
+                .httpBasic(withDefaults())
+                .build();
     }
 }
